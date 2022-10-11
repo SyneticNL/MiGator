50f5365ed83e22bbeafa6769cbfc4c6a0f4bdc3c
tomvanetten <tom.vanetten@synetic.nl>
2022-10-11 14:00:52 +0200

    feat(kapstok): Initial scaffold
    - added writer

--- tests/Feature/WriterTest.php
+++ tests/Feature/WriterTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Synetic\Migator\Tests\Commands;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
 use Synetic\Migator\Service\Writer\Writer;
 use Synetic\Migator\Tests\TestCase;
 
@@ -11,6 +13,8 @@ class WriterTest extends TestCase
 {
 
     private Writer $writer;
+
+    private string $testString = 'Schema::create(\'users\', static function (Blueprint $table) {$table->id(); $table->string(\'name\');});';
 
     protected function setUp(): void
     {
@@ -22,8 +26,33 @@ class WriterTest extends TestCase
     public function test_writer_formatter_empty_collection(): void
     {
         $build = collect();
-        $result = $this->writer->formatBuilderCollectionToCreateSchemaCollection($build);
+        $result = $this->writer->formatBuilderCollectionToUp($build);
         $this->assertSame($result , '');
+    }
+
+    public function test_writer_formatter(): void
+    {
+        $build = collect(['users' => collect([
+            'id()',
+            'string(\'name\')'
+        ])]);
+
+        $result = $this->writer->formatBuilderCollectionToUp($build);
+        $this->assertSame(
+            $this->testString
+            ,$result
+        );
+    }
+
+    public function test_replace_in_template(): void
+    {
+        $result = $this->writer->createMigration($this->testString, collect(['users' , 'typos']));
+        $this->assertSame(File::get(__DIR__ .'/Files/WriterTestResult.php'), $result);
+    }
+
+    public function test_migration_name(): void
+    {
+        $this->assertSame('migration.php', $this->writer->getMigrationName());
     }
 
 }
