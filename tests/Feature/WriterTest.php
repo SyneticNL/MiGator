120b39fb531ab2d623893474afe5ff55dcfbf100
tomvanetten <tom.vanetten@synetic.nl>
2022-10-11 14:55:24 +0200

    feat(kapstok): Writer name convention
    Migration with cool new names

--- tests/Feature/WriterTest.php
+++ tests/Feature/WriterTest.php
@@ -48,6 +48,6 @@ class WriterTest extends TestCase
 
     public function test_migration_name(): void
     {
-        $this->assertSame('migration.php', $this->writer->getMigrationName());
+        $this->assertContains('user_name_migration.php', $this->writer->getMigrationName(collect(['user', 'name'])));
     }
 }
