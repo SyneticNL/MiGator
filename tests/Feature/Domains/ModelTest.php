6b1c685150a23a17dac1b528bec523f748816b1c
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2022-10-11 16:08:38 +0200

    feat(test): added column test

--- tests/Feature/Domains/ModelTest.php
+++ tests/Feature/Domains/ModelTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Synetic\Migator\Tests\Feature\Domains;
 
+use Synetic\Migator\Domains\Field;
+use Synetic\Migator\Domains\FieldTypes\StringType;
 use Synetic\Migator\Domains\Model;
 use Synetic\Migator\Tests\TestCase;
 
@@ -24,5 +26,26 @@ class ModelTest extends TestCase
     public function testCanDetect($table, $expected): void
     {
         static::assertEquals($expected, (new Model($table))->exists());
+    }
+
+    public function providerTestCanDetectFieldsExist(): array
+    {
+        return [
+            'can detect column exists on table' => ['user', 'name', true],
+            'can detect column does not eist on table' => ['user', 'random', false],
+        ];
+    }
+
+    /**
+     * @dataProvider providerTestCanDetectFieldsExist
+     */
+    public function testCanDetectFieldsExist($table, $field, $expected): void
+    {
+        $model = new Model($table);
+        static::assertEquals($expected, $model->columnExists($field));
+        if (!$expected) {
+            $model->addField(new Field($field, new StringType));
+            static::assertEquals(true, $model->columnExists($field));
+        }
     }
 }
