16e5f65be0ae9e96c9e78757f1fc3bef136387e2
Robin Speekenbrink <robin.speekenbrink@synetic.nl>
2022-10-11 11:20:51 +0200

    feat: first test

---
+++ tests/Commands/AboutCommandTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+class AboutCommandTest extends \Synetic\Migator\Tests\TestCase {
+
+  public function test_it_has_the_about_signature(): void {
+    $this->artisan('migator:about')
+      ->assertExitCode(0);
+  }
+
+}
