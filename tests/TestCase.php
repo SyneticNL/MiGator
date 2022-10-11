2c9869d1d3b93c6f2402f43efcefe68f13f0c62d
Robin Speekenbrink <robin.speekenbrink@synetic.nl>
2022-10-11 11:16:58 +0200

    feat: tests stub

---
+++ tests/TestCase.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Synetic\Migator\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+use Synetic\Migator\MigatorServiceProvider;
+
+class TestCase extends Orchestra
+{
+  protected function getPackageProviders($app): array {
+    return [
+      MigatorServiceProvider::class,
+    ];
+  }
+}
