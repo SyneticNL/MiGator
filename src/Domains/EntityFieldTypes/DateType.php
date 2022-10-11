ec2470f9cfea5d3c0741ba2486310a6773c09672
Jordy Vleugel <jordy@synetic.nl>
2022-10-11 14:19:41 +0200

    feat: add dateTime

--- src/Domains/EntityFieldTypes/DateType.php
+++ src/Domains/EntityFieldTypes/DateType.php
@@ -8,8 +8,8 @@ use Synetic\Migator\Domains\EntityTypeInterface;
 
 class DateType implements EntityTypeInterface
 {
-    public function toMigrationString(): string
+    public function toMigrationString(string $column): string
     {
-        return '';
+        return sprintf('$table->date(\'%s\')', $column);
     }
 }
