e16b9a61ed648be817f0bcf337a2043efbadffbc
Misha van Tol <misha.vantol@synetic.nl>
2022-10-16 22:43:01 +0200

    fix: id type can have empty (default 'id') column

--- src/Domains/FieldTypes/AbstractFieldType.php
+++ src/Domains/FieldTypes/AbstractFieldType.php
@@ -34,14 +34,22 @@ abstract class AbstractFieldType implements FieldTypeInterface, \Stringable
 
     public function toMigrationString(string $column): string
     {
+        if ($this instanceof IdType && $column === 'id') {
+            $column = '';
+        }
+
+        if ($column) {
+            $column = '\'' . $column . '\'';
+        }
+
         return Str::of($this->method)
-            ->append(sprintf('(\'%s\'', $column))
+            ->append('(' . $column)
             ->when($this->getParameters()->isNotEmpty(), function (Stringable $string) {
                 return $string
                     ->append(', ')
                     ->append(
                         $this->getParameters()
-                            ->map(fn ($value, $key) => sprintf('$%s = %s', $key, $value ?? 'null'))
+                            ->map(fn($value, $key) => sprintf('$%s = %s', $key, $value ?? 'null'))
                             ->join(', ')
                     );
             })
@@ -64,7 +72,7 @@ abstract class AbstractFieldType implements FieldTypeInterface, \Stringable
             $default = $default ? 'true' : 'false';
         }
 
-        return (string) ($default ?? 'null');
+        return (string)($default ?? 'null');
     }
 
     public function __toString(): string
